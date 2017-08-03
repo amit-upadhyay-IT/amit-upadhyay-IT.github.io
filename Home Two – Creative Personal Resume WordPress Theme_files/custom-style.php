3adab1f5e5283d5c02a7399b83d0cdd2c00e8205
amit-upadhyay-IT <youarefreak1@gmail.com>
2017-08-03 11:17:15 +0530

    updated

---
+++ Home Two – Creative Personal Resume WordPress Theme_files/custom-style.php
@@ -0,0 +1,256 @@
+
+body{
+    background-color: #ffffff;
+    color: #282828;
+    font-size: 14px;
+    font-family: Roboto, sans-serif;
+    font-weight: 400;
+    line-height: 28px;
+}
+
+h1, 
+h2, 
+h3, 
+h4, 
+h5, 
+h6{
+    color: #282828;
+    font-family: Roboto, sans-serif;
+    font-weight: 700;
+}
+h1{
+    font-size: 36px;
+    line-height: 36px;
+}
+h2{
+    font-size: 30px;
+    line-height: 30px;
+}
+h3{
+    font-size: 24px;
+    line-height: 24px;
+}
+h4{
+    font-size: 18px;
+    line-height: 18px;
+}
+h5{
+    font-size: 14px;
+    line-height: 18px;
+}
+h6{
+    font-size: 12px;
+    line-height: 16px;
+}
+
+.header-wrapper,
+.navbar-default {
+    ;
+}
+
+.sticky-wrapper.is-sticky .header-wrapper,
+.sticky-wrapper.is-sticky .navbar-default{
+    ;
+}
+
+.navbar-default .navbar-nav>li>a{
+    ;
+}
+
+.sticky-wrapper.is-sticky .navbar-default .navbar-nav>li>a{
+    ;
+}
+
+a,
+a:focus{
+    color: #68c3a3;
+}
+
+a:hover {
+    color: #369171;
+}
+
+
+
+.btn-primary,
+.social-icons a i:hover::after,
+.navbar-default .navbar-toggle:focus, 
+.navbar-default .navbar-toggle:hover,
+.timeline>li .timeline-content:hover,
+.progress-bar,
+#filter li a.active,
+#filter li a:hover,
+.links a:hover i::after,
+.portfolio-info,
+.figure i:after,
+.scroll-up a,
+.widget_archive > ul > li::before,
+.widget_categories > ul > li::before,
+#menu-issue-category > li::before,
+.widget_mc4wp_form_widget,
+.tagcloud a:hover,
+.pagination>li>a:focus,
+.pagination>li>a:hover,
+.pagination>li>span:focus,
+.pagination>li>span:hover,
+.pagination>li>span.current,
+.single-post-navigation a:hover,
+.comment-author .comment-reply-link:hover,
+.comment-author .comment-reply-login:hover,
+.page-pagination a:hover,
+.page-pagination > span,
+.social-links li a:hover,
+#toTop:hover{
+    background-color: #68c3a3;
+}
+
+
+
+
+.btn-primary:hover, 
+.btn-primary:focus, 
+.btn-primary.focus, 
+.btn-primary:active,
+.btn-primary:active:focus,
+.btn-primary.active, 
+.open>.dropdown-toggle.btn-primary{
+    background-color: #369171;
+}
+
+
+
+.btn-default:hover, 
+.btn-default:focus, 
+.btn-default.focus, 
+.btn-default:active, 
+.btn-default:active:focus, 
+.btn-default.active,
+ul.list-check li::before,
+.intro h1 span,
+.navbar-default .navbar-nav li a:focus,
+.navbar-default .navbar-nav li a:hover,
+.navbar-default .navbar-nav>.active>a, 
+.navbar-default .navbar-nav>.active>a:focus, 
+.navbar-default .navbar-nav>.active>a:hover,
+.navbar-default .navbar-nav>.open>a, 
+.navbar-default .navbar-nav>.open>a:focus, 
+.navbar-default .navbar-nav>.open>a:hover,
+.dropdown-menu>.active>a, 
+.dropdown-menu>.active>a:focus, 
+.dropdown-menu>.active>a:hover,
+.navbar-default .navbar-nav li.current-menu-ancestor.has-mega-menu-child>a:hover,
+.navbar-default .navbar-nav li.current-menu-parent.has-mega-menu-child>a:hover,
+.dropdown-menu>li>a:focus, 
+.dropdown-menu>li>a:hover,
+.video-intro a:hover,
+#filter li a,
+.links a i,
+.entry-title a:hover,
+.entry-meta ul li a:hover,
+.widget .entry-meta ul li a:hover,
+.personal-info h1 span,
+.colored,
+.widget .entry-meta ul li a:hover,
+.blog-wrapper .entry-meta ul li a:hover,
+.widget a:hover,
+.widget-title,
+.tt-recent-comments .comment-content .comment-title a:hover,
+.nav-tabs>li.active>a,
+.nav-tabs>li.active>a:focus,
+.nav-tabs>li.active>a:hover,
+.nav-tabs>li>a:hover,
+.nav-tabs>li>a:focus,
+.tt-popular-post h4 a:hover
+.widget_mc4wp_form_widget .btn:hover,
+.widget_mc4wp_form_widget .btn:focus,
+.tags-links a:hover,
+.latest-post-carousel .entry-title a:hover,
+.latest-post-wrapper .entry-meta ul li a:hover,
+.latest-post-wrapper .entry-title a:hover,
+.all-category-links a:hover,
+#toTop,
+.error-message h2{
+    color: #68c3a3;
+}
+
+
+.navbar-default .navbar-nav li.current-menu-ancestor>a,
+.navbar-default .navbar-nav li.current-menu-parent>a,
+.navbar-default .navbar-nav li.current-menu-item>a{
+    color: #68c3a3 !important;
+}
+
+
+
+
+.form-control:focus,
+.navbar-default .navbar-toggle:focus, 
+.navbar-default .navbar-toggle:hover,
+.links a i::after,
+.widget_archive > ul ul li::before,
+.widget_categories > ul ul li::before,
+#menu-issue-category > ul li::before,
+.tagcloud a:hover,
+.single-post-navigation a,
+#toTop{
+    border-color: #68c3a3;
+}
+
+.timeline>li .timeline-panel:hover::before,
+.timeline>li .timeline-panel:hover::after{
+    border-left-color: #68c3a3;
+}
+
+.timeline>li.timeline-inverted .timeline-panel:hover::before,
+.timeline>li.timeline-inverted .timeline-panel:hover::after{
+    border-right-color: #68c3a3;
+}
+
+#filter li a.active::after, 
+#filter li a:hover::after{
+    border-top-color: #68c3a3;
+}
+
+
+.btn-primary:hover, 
+.btn-primary:focus, 
+.btn-primary.focus, 
+.btn-primary:active,
+.btn-primary:active:focus,
+.btn-primary.active, 
+.open>.dropdown-toggle.btn-primary{
+    border-color: #369171;
+}
+
+
+
+
+@media (max-width : 767px) {
+    .navbar-default{
+         !important;
+    }
+
+    .sticky-wrapper.is-sticky .navbar-default .navbar-default {
+         !important;
+    }
+
+    .navbar-default .navbar-nav li a{
+        ;
+    }
+
+    .navbar-default .navbar-nav .open .dropdown-menu>li>a:focus, 
+    .navbar-default .navbar-nav .open .dropdown-menu>li>a:hover,
+    .navbar-default .navbar-nav .open .dropdown-menu>.active>a, 
+    .navbar-default .navbar-nav .open .dropdown-menu>.active>a:focus, 
+    .navbar-default .navbar-nav .open .dropdown-menu>.active>a:hover,
+    .dropdown-menu-trigger.menu-collapsed{
+        color: #68c3a3;
+    }
+
+    .dropdown-menu-trigger.menu-collapsed{
+        border-color: #68c3a3;
+    }
+
+
+
+}
